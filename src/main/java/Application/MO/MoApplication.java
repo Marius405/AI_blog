3bc2f28f65f3abc9482297581a1b09911ce682e1
Marius405 <mariusrabe94@gmail.com>
2023-05-08 18:10:15 +0300

    Main

--- src/main/java/Application/MO/MoApplication.java
+++ src/main/java/Application/MO/MoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.List;
 
@@ -17,15 +17,7 @@ public class MoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
+
 
 	@GetMapping("/huhu")
 	public String message(){
